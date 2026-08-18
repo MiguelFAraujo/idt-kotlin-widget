18da7227c4f7ebe0150dfe57ab639bdff38a1c8f
miguel <miguel@idt-lab>
2026-08-18 20:44:35 -0300

    v1.0.4: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.idt.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.3"
+        versionCode = 15
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
